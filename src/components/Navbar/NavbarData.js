a9fb8165b4db39d2dd40f237705970124bd82857
vikasntechzy <vikas@ntechzy.in>
2024-11-04 18:05:18 +0530

    first commit

---
+++ src/components/Navbar/NavbarData.js
@@ -0,0 +1,39 @@
+export const navbarData = [
+    {
+      "label": "Home",
+      "link": "/"
+    },
+    {
+      "label": "Services",
+      "link": "/services",
+      "submenu": [
+        {
+          "label": "Web Development",
+          "link": "/services/web-development"
+        },
+        {
+          "label": "App Development",
+          "link": "/services/app-development"
+        }
+      ]
+    },
+    {
+      "label": "About",
+      "link": "/about",
+      "submenu": [
+        {
+          "label": "Web Development",
+          "link": "/services/web-development"
+        },
+        {
+          "label": "App Development",
+          "link": "/services/app-development"
+        }
+      ]
+    },
+    {
+      "label": "Contact",
+      "link": "/contact"
+    }
+  ]
+  
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Banner from '../components/BannerComponent/Banner'
+import AppointmentForm from '../components/Home/AppointmentForm'
+
+const Contact = () => {
+  return (
+    <div>
+        <Banner CapitalPagename="CONTACT US" pagename="Contact Us"/>
+        <AppointmentForm/>
+    </div>
+  )
+}
+
+export default Contact
