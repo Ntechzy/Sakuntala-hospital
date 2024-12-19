a5ee74e7c2a4e25b74154500ae00eaa05d651583
vikasntechzy <vikas@ntechzy.in>
2024-12-19 11:52:52 +0530

    resize banner

--- src/components/Home/HomeBanner.jsx
+++ src/components/Home/HomeBanner.jsx
@@ -7,15 +7,15 @@ const HomeBanner = () => {
       {/* Header Section */}
 
       {/* Doctor Section */}
-      <div className=" flex flex-col md:flex-row items-center justify-between mt-24 relative space-y-4 md:space-y-0">
+      <div className=" flex flex-col md:flex-row items-center justify-between object-cover mt-20   relative space-y-4 md:space-y-0 md:bg-url('/assets/gallery/Banner.jpg')">
         {/* Doctor's Image */}
-        <img className="" src="/assets/banner.jpg" alt="" />
+        <img className="" src="/assets/gallery/Banner.jpg" alt="" />
       </div>
 
       {/* Footer */}
-      <div className="bg-gray-800 text-white text-center md:p-4 p-2 gap-2 md:absolute bottom-0 left-12 md:w-[61%] md:text-lg flex flex-col md:flex-row justify-center md:gap-11 px-4 md:px-0">
+      {/* <div className="bg-gray-800 text-white text-center md:p-4 p-2 gap-2 md:absolute bottom-0 left-12 md:w-[61%] md:text-lg flex flex-col md:flex-row justify-center md:gap-11 px-4 md:px-0">
         <p> एक ही छत के नीचे Fertility इलाज की सारी सुविधाएं पाएं</p>
-      </div>
+      </div> */}
 
       {/* WhatsApp Icon */}
       
